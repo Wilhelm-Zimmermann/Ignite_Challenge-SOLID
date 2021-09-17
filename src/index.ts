960a61f672762791a1bcb27542866256da134bfb
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-09-17 10:47:57 -0300

    Swagger added, Documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerUrl from "./swagger.json";
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerUrl));
 
 app.use("/users", usersRoutes);
 
